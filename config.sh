47ce1085fff65f28a0b5c84ed9d7565bce1bba59
Andrei-Parfenov <Parfenov@gmail.com>
2022-01-05 18:55:54 -0500

    Создание скрипта config согласно плану

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+   # База данных
+   rootpass="password123"
+   dbname="dbnames"
+   dbuser="dbrandomuser"
+   userpass="userpassord123"
+
+   # Путь к директории сайта /var/www/****
+   dirsite="namesite"
+
+   # Файл конфигурации Apache2 /etc/apache2/sites-available:
+   nameconf="namesite.com"
+   path="/etc/apache2/sites-available"
+   adressite="wp-test.org"
