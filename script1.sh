6eae592383a165ad47797a015518975ea06b941c
Andrei-Parfenov <Parfenov@gmail.com>
2022-01-05 18:55:20 -0500

    Создание и изменения скрипта №1

--- script1.sh
+++ script1.sh
@@ -2,10 +2,8 @@
 # Установка WordPress Debian11
 # Скрипт 1 подготовка среды для установки WordPress
 # Не забываем давать права скрипту (chmod +x ./ИМЯ_ФАЙЛА)
-sudo apt-get upgrade
-sudo apt-get update
-sudo apt install apache2 apache2-utils
-sudo apt-get install mariadb-client mariadb-server
-read -p "Далее нужно произвезти настройку безопасности Базы Данных нажми Enter"
-sudo mysql_secure_installation
-sudo apt-get install php7.4 php7.4-mysql libapache2-mod-php7.4 php7.4-cli php7.4-cgi php7.4-gd
+sudo apt update
+sudo apt -f -y install apache2 apache2-utils
+sudo apt -f -y install mariadb-client mariadb-server
+sudo ./script3.sh
+sudo apt-get -f -y install php7.4 php7.4-mysql libapache2-mod-php7.4 php7.4-cli php7.4-cgi php7.4-gd
