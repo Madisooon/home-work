e23a78bb6176e12ba41538dd709a66928384ff1b
Andrei-Parfenov <Parfenov@gmail.com>
2022-01-08 04:12:41 -0500

    add finish script1

--- script1.sh
+++ script1.sh
@@ -8,3 +8,8 @@ sudo apt -f -y install mariadb-client mariadb-server
 sudo ./script3.sh
 sudo apt-get -f -y install php7.4 php7.4-mysql libapache2-mod-php7.4 php7.4-cli php7.4-cgi php7.4-gd
 sudo ./script2.sh
+sudo ./script4.sh
+source config.sh
+sudo echo 127.0.0.1 $adressite >> /etc/hosts
+sudo echo 127.0.0.1 www.$adressite >> /etc/hosts
+echo Всё Сделано босс! Далее нужно настроить сайт $adressite в браузере.
