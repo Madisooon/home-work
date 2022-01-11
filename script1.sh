86b68e75d737ab0553b1cbf793a334354136a2e8
Andrei Parfenov <123@mail.ru>
2022-01-11 11:11:08 -0500

    fix sudo

--- script1.sh
+++ script1.sh
@@ -10,6 +10,6 @@ sudo apt -f -y install php7.4 php7.4-mysql libapache2-mod-php7.4 php7.4-cli php7
 sudo ./script2.sh
 sudo ./script4.sh
 source config.sh
-sudo echo 127.0.0.1 $adressite >> sudo /etc/hosts
-sudo echo 127.0.0.1 www.$adressite >> sudo /etc/hosts
+sudo echo 127.0.0.1 $adressite >> /etc/hosts
+sudo echo 127.0.0.1 www.$adressite >> /etc/hosts
 echo Всё Сделано босс! Далее нужно настроить сайт $adressite в браузере.
