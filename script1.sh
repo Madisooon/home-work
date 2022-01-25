8238748f2b9ab39720ff8ededd871dbabf647217
Madisooon <96781339+Madisooon@users.noreply.github.com>
2022-01-25 17:24:32 +0300

    RootFix

--- script1.sh
+++ script1.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 # Установка WordPress Debian
 # Скрипт 1 подготовка среды для установки WordPress
-# Не забываем давать права скрипту (chmod +x ./ИМЯ_ФАЙЛА)
+# Выполняем этот скрипт от Root-пользователя
+su
 sudo apt update
-sudo apt -f -y install apache2 apache2-utils
-sudo apt -f -y install mariadb-client mariadb-server
-sudo ./script3.sh
-sudo apt -f -y install php7.4 php7.4-mysql libapache2-mod-php7.4 php7.4-cli php7.4-cgi php7.4-gd
-sudo ./script2.sh
-sudo ./script4.sh
-source config.sh
-sudo echo 127.0.0.1 $adressite >> /etc/hosts
-sudo echo 127.0.0.1 www.$adressite >> /etc/hosts
+apt -f -y install apache2 apache2-utils
+apt -f -y install mariadb-client mariadb-server
+# Скрипт для создания базы данных в mariaDB
+./script3.sh
+apt -f -y install php7.4 php7.4-mysql libapache2-mod-php7.4 php7.4-cli php7.4-cgi php7.4-gd
+# Скрипт для скачивания WP с сайта и создание папки в директории var/www/
+./script2.sh
+# Скрипт для Создания файла сборки для Apache2 и даем ссылку на папку с включенным
+./script4.sh
+# Далее выполняем если необходимо зайти локально с виртуалки 
+# source config.sh
+# sudo echo 127.0.0.1 $adressite >> /etc/hosts
+# sudo echo 127.0.0.1 www.$adressite >> /etc/hosts
 echo Всё Сделано босс! Далее нужно настроить сайт $adressite в браузере.
